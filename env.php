35655939d691b43cdb937ce43c2f90aacfc363c5
itxiao6 <843161352@qq.com>
2016-12-28 14:01:06 +0800

    忽略文件修复

---
+++ env.php
@@ -0,0 +1,22 @@
+<?php
+// ENV 环境配置文件(用来适应不同的开发环境)
+return [
+	// 数据库主机
+	'dbhost'=>'localhost',
+	// 数据库名称
+	'database'=>'',
+	// 数据库用户名
+	'username'=>'',
+	// 数据库密码
+	'password'=>'',
+	// session储存方式
+	'session'=>'file',// file\database
+	// log储存方式
+	'log'=>'file',// file\database
+	// 七牛云存储参数
+    'Bucket_Name'=>'',// 空间名称
+    'accessKey'=>'',// 空间名称
+    'secretKey'=>'',// 空间名称
+
+
+];
