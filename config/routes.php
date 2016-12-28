f615a96deb5c74eee19c4ba3ef8a45b5ad8c86e9
itxiao6 <843161352@qq.com>
2016-12-28 12:12:57 +0800

    this is  a test

--- config/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Config;
-use Service\Route;
-// 前台首页
-Route::get('/','Home\HomeController@home');
-
-
-// 初始化欢迎页
-Route::get('/Admin/Welcome/index.html','Admin\WelcomeController@index');
