8d67cf1f79c3ac9de0cc99842c2d9a3826d99440
戒尺 <843161352@qq.com>
2017-05-09 10:54:31 +0800

    后台资源文件虚拟目录

--- config/abstract.php
+++ config/abstract.php
@@ -3,4 +3,6 @@
 return [
 	# debugbar资源文件
 	'resources'=>ROOT_PATH.'vendor'.DIRECTORY_SEPARATOR.'maximebf'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar',
+	# 后台资源文件
+	'assets'=>ROOT_PATH.'common'.DIRECTORY_SEPARATOR.'maximebf'.DIRECTORY_SEPARATOR.'debugbar'.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'DebugBar',
 ];
