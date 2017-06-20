9b055101550b7925dc5d9785e3c25b182b31c3c3
15538147923 <843161352@qq.com>
2017-06-20 16:39:20 +0800

    配置文件
    配置文件

--- config/wechat.php
+++ config/wechat.php
@@ -1,17 +1,17 @@
 <?php
 return [
 	# 微信的公众平台的appid
-	'appid'=>'',
+	'appid'=>'wxb66de5b7b930111',
 	# 微信开放平台的的appid
 	'open_appid'=>'',
 	# 公众号的secret
-	'secret'=>'',
+	'secret'=>'e05a8b1c00d2abd93b92142214227622',
 	# 微信公众号商户平台付key
-	'pay_key'=>'',
+	'pay_key'=>'1abb3521ceb70ef277bec80491287ff23',
 	# 微信开放商户平台key
 	'open_pay_key'=>'',
 	# 微信公众商户平台商户id
-	'mchid' => '',
+	'mchid' => '1468543702',
 	# 微信开放平台商户id
 	'open_mchid' => '',
 	#通知回调地址
