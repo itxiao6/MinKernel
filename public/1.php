ded1736d292c6b8e729b14f6ca2877b07ea0c9a0
刘广财 <itxiao6@qq.com>
2017-07-07 14:57:30 +0800

    测试残留

--- public/1.php
+++
@@ -1,5 +0,0 @@
-<?php
-$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb66de5b8f7b93011&redirect_uri=http://'.$_SERVER['HTTP_HOST'].'/2.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
-header('Location:'.$url);
-// $result = file_get_contents($url);
-// var_dump($result);
