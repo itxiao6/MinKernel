2eee7ddfab8635a9a419fbbbb36c2d70b3db384f
刘广财 <itxiao6@qq.com>
2017-07-07 18:06:25 +0800

    无用资源删除

--- public/js/zpf-login.js
+++
@@ -1,9 +0,0 @@
-	document.documentElement.style.fontSize = innerWidth / 16 + 'px';
-
-		onresize = function() {
-			document.documentElement.style.fontSize = innerWidth / 16 + 'px';
-		};
-
-  $(".zpf-dian").click(function(){
-  	window.location.href="wy_index.html";
-  })
