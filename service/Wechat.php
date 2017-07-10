3ca378940de3dbb1dd53e8ca2c866b342628e4d0
刘广财 <itxiao6@qq.com>
2017-07-10 15:28:38 +0800

    微信功能类整理文档

--- service/Wechat.php
+++ service/Wechat.php
@@ -52,38 +52,13 @@ class Wechat{
 	# 红包实例
 	protected static $cash = false;
 
-	// 调用例子 Wechat::transfersqiye('ojmuBwdCKPD5xrYG41bIuy_iDzlw',1.18,ROOT_PATH.'pem/apiclient_cert.pem',ROOT_PATH.'pem/apiclient_key.pem',$desc='企业转账',$check_name='NO_CHECK',$re_user_name='李先生');
-	# 微信企业到账
-	public static function transfersqiye($openid,$money,$cert,$sslKey,$desc='企业转账',$check_name='NO_CHECK',$re_user_name=''){
-		# 判断要发送的红包金额是否小于1元
-		if($money < 1){
-			# 发送的红包金额不能小于1元
-			return false;
-		}
-		# 初始化企业转账类
-		$transfers = new Transfers(C('appid','wechat'), C('mch_id','wechat'), C('key','wechat'));
-
-		# 企业转账必需设置证书
-		$transfers->setSSLCert($cert,$sslKey);
-
-		# 设置企业转账信息
-		$transfers->set('partner_trade_no',rand(0,20).date('YmdHisyysssi').rand(0,20));# 转账者
-		$transfers->set('openid',$openid);# 转账者
-		$transfers->set('check_name',$check_name);# 转账者
-		$transfers->set('re_user_name',$re_user_name);# 转账者
-		$transfers->set('amount',$money*100);# 转账者
-		$transfers->set('desc',$desc);# 转账者
-		$transfers->set('spbill_create_ip',$_SERVER['SERVER_ADDR']);# 转账者
-
-		try {
-		    $response = $transfers->getResponse();
-		} catch (\Exception $e) {
-		    exit($e->getMessage());
-		}
-		# 返回发放结果
-		return $response->toArray();
-	}
-	# 微信app支付
+    /**
+     * 微信APP支付
+     * @param $pay_order_num 支付订单号
+     * @param $order_name 订单名称
+     * @param $order_price 订单金额
+     * @return 下单生成的数据集合
+     */
 	public static function app_pay($pay_order_num,$order_name,$order_price){
 		# 初始化微信统一下单SDK(Appid,商户平台id,商户秘钥）参数来自于微信开放平台
 		$unifiedorder = new Unifiedorder(C('open_appid','wechat'),C('open_mchid','wechat'),C('openid_pay_key','wechat'));
@@ -123,40 +98,14 @@ class Wechat{
         $two_sign = self::getSign($resignData);
         # 拼接二次签名的数据集合(用来返回到APP）
 		$result_data = '{"appid":"'.$result_unifiedorder['appid'].'","partnerid":"'.$result_unifiedorder['mch_id'].'","package":"Sign=WXPay","noncestr":"'.$result_unifiedorder['nonce_str'].'","timestamp":"'.$now_time.'","prepayid":"'.$result_unifiedorder['prepay_id'].'","sign":"'.$two_sign.'"}';
-		# 分配下单结果给模板引擎
-		$this -> ajaxReturn(['status'=>1,'data'=>$result_data,'message'=>'请求成功']);
+		# 返回数据集合
+		return $result_data;
 	}
-	/**
-     * 获取参数签名；
-     * @param  Array  要传递的参数数组
-     * @return String 通过计算得到的签名；
+
+    /**
+     * 获取accessToken
+     * @return String 微信的Access_Token
      */
-    protected static function getSign($params) {
-        #将参数数组按照参数名ASCII码从小到大排序
-        ksort($params);
-        # 循环处理要签名的数组
-        foreach ($params as $key => $item) {
-            # ß剔除参数值为空的参数
-            if (!empty($item)) {         
-                # 整合新的参数数组
-                $newArr[] = $key.'='.$item;     
-            }
-        }
-        # 使用 & 符号连接参数
-        $stringA = implode("&", $newArr);         
-        # 拼接key
-        # key是在商户平台API安全里自己设置的
-        $stringSignTemp = $stringA."&key=".C('openid_pay_key','wechat');        
-
-        # 将字符串进行MD5加密
-        $stringSignTemp = MD5($stringSignTemp);       
-        # 将所有字符转换为大写
-        $sign = strtoupper($stringSignTemp);
-        # 返回签名结果     
-        return $sign;
-    }
-
-	# 获取accessToken
 	public static function get_access_token(){
 		if(self::$cacheDriver==false){
 			self::$cacheDriver = new FilesystemCache(CACHE_DATA);
@@ -168,8 +117,13 @@ class Wechat{
 		# 返回字符串类型的AccessToken
 		return self::$accessToken->getTokenString();
 	}
-	# 获取JSapi
-	public static function get_jsapi($api,$debug=false){
+    /**
+     * 获取微信JSAPI
+     * @param Array $api API的列表
+     * @param bool $debug 是否开启调试模式
+     * @return $this|jsapi
+     */
+	protected static function get_jsapi($api,$debug=false){
 		# 判断是否已经获取过accessToken
 		if(self::$accessToken==false){
 			# 获取accessToken
@@ -190,19 +144,30 @@ class Wechat{
 			return self::$jsapi;
 		}
 	}
-	# 获取JSAPI配置
-	public static function get_jsapi_config($api='',$is_array=false,$debug=false){
+
+    /**
+     * 获取微信JSAPI 配置
+     * @param Array $api JSAPI 列表
+     * @param bool $is_array 是否返回
+     * @param bool $debug 是否为调试模式
+     * @return mixed 可为数组 可为 字符串
+     */
+	public static function get_jsapi_config($api=[],$is_array=false,$debug=false){
 		# 判断是否已经获取过JSAPI
 		if(self::$jsapi==false){
-			// dump($api);die();
 			# 获取accessToken
 			self::get_jsapi($api,$debug);
-
 		}
 		# 返回JSAPI json字符串
 		return self::$jsapi->getConfig(($is_array==true)?true:false);
 	}
-	# 获取带参数的二维码
+
+    /**
+     * 获取微信二维码
+     * @param $param 二维码带的参数数据
+     * @param int $out_time 过期时间 单位为毫秒 0 则表示永久二维码
+     * @return string 返回二维码链接地址
+     */
 	public static function get_Qrcode($param,$out_time=0){
 		# 判断是否已经获取过accessToken
 		if(self::$accessToken==false){
@@ -223,6 +188,11 @@ class Wechat{
 		}
 	}
 	# 获取微信服务器IP
+
+    /**
+     * 获取微信服务器ip 列表 (用于安全监测)
+     * @return mixed 返回微信服务器 IP 列表
+     */
 	public static function get_Wechat_Server_IP(){
 		# 判断是否已经获取过accessToken
 		if(self::$accessToken==false){
@@ -236,7 +206,11 @@ class Wechat{
 		# 返回微信服务器ip列表
 		return self::$serverIp->getIps();
 	}
-	# 生成短连接
+    /**
+     * 生成微信短链接(参数过多用于解决GET限制)
+     * @param $url 源URl
+     * @return mixed 转换的结果
+     */
 	public static function toShort($url){
 		# 判断是否已经获取过accessToken
 		if(self::$accessToken==false){
@@ -250,7 +224,11 @@ class Wechat{
 		# 返回短连接
 		return $shortUrl->toShort($url);
 	}
-	# 获取用户授权信息
+    /**
+     * 获取用户access_token
+     * @param $callBack 回调地址
+     * @return 返回用户的access_token
+     */
 	public static function get_Web_user_Access_Token($callBack){
 		# 判断是否已经生成过了
 		if(self::$user_accessToken != false){
@@ -275,7 +253,11 @@ class Wechat{
 		# 返回用户授权(可toArray())
 		return self::$user_accessToken;
 	}
-	# 获取用户信息
+    /**
+     * 获取用户信息
+     * @param $callBack 回调地址
+     * @return bool 返回用户的信息
+     */
 	public static function get_user_info($callBack){
 		if(self::$user_accessToken==false || (empty($_GET['code']) && empty($_GET['state'])) ){
 			self::get_Web_user_Access_Token($callBack);
@@ -293,7 +275,11 @@ class Wechat{
 		# 返回用户信息(可以toArray)
 		return self::$userinfo;
 	}
-	# 统一下单
+    /**
+     * 微信统一下单
+     * @param array $data 下单数据
+     * @return 下单结果
+     */
 	public static function Unifiedorder($data = []){
 		# 初始化下单接口
 		self::$unifiedorder = new Unifiedorder(C('appid','wechat'),C('mchid','wechat'),C('pay_key','wechat'));
@@ -302,26 +288,6 @@ class Wechat{
 			# 设置订单内容
 			self::$unifiedorder->set($key,$value);
 		}
-		/* 参考
-		# 必填
-		$unifiedorder->set('body',          '微信支付测试商品');
-		$unifiedorder->set('total_fee',     1);
-		$unifiedorder->set('openid',        'OPENID');
-		$unifiedorder->set('trade_type',    'JSAPI');
-		$unifiedorder->set('out_trade_no',  date('YmdHis').mt_rand(10000, 99999));
-		$unifiedorder->set('notify_url',    'http://example.com/your-notify.php');
-
-		# 选填
-		$unifiedorder->set('device_info',   'WEB');
-		$unifiedorder->set('detail',        '商品详情');
-		$unifiedorder->set('attach',        '自定义附加数据');
-		$unifiedorder->set('fee_type',      'CNY');
-		$unifiedorder->set('time_start',    '20091225091010');
-		$unifiedorder->set('time_expire',   '20091227091010');
-		$unifiedorder->set('goods_tag',     'WXG');
-		$unifiedorder->set('limit_pay',     'no_credit');
-
-		 */
 		try {
 		    $response = self::$unifiedorder->getResponse();
 		} catch (\Exception $e) {
@@ -330,7 +296,11 @@ class Wechat{
 		# 返回下单结果
 		return $response;
 	}
-	# 生成 ChooseWXPay 配置文件
+    /**
+     * 生成公众号支付配置
+     * @param array $data 下单数据
+     * @return PayChoose 公众号数据
+     */
 	public static function ChooseWXPay($data=[]){
 		# 判断是否已经下单
 		if(self::$unifiedorder==false){
@@ -339,37 +309,12 @@ class Wechat{
 		}
 		# 获取配置
 		return self::$chooseWXPayConfig = new PayChoose(self::$unifiedorder);
-		/*
-			<button type="button" onclick="WXPayment()"> 支付 ￥<?php echo ($unifiedorder['total_fee']/100); ?> 元</button>
-		 */
-
-		/*
-		<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-		<script>
-		#
-		# 注入 Jsapi 配置
-		#
-		wx.config(<?php echo $jsapi; ?>);
-
-		#
-		# 点击支付按款
-		#
-		var WXPayment = function() {
-
-		    var config = <?php echo $config; ?>;
-
-		    config.success = function() {
-		        alert('支付成功！');
-		    }
-
-		    config.cancel = function() {
-		        alert('用户取消成功！');
-		    }
-
-		    wx.chooseWXPay(config);
-		}
-		</script>*/
 	}
+
+    /**
+     * 微信异步通知回调
+     * @param $callBack 微信支付逻辑处理回调
+     */
 	public static function notitfy($callBack){
 		# 初始化回调通知类
 		$notify = new Notify();
@@ -378,9 +323,6 @@ class Wechat{
 		    # 失败时必需返回，否则微信服务器将重复提交通知
 		    $notify->fail('Invalid Request');
 		}
-
-		# 在这里处理你的业务逻辑，比如修改订单状态
-		# $notify['out_trade_no'] 为订单号
 
 		# 调用回调方法
 		if($callBack($notify)){
@@ -391,6 +333,20 @@ class Wechat{
 			$notify -> fail('FAIL');
 		}
 	}
+
+    /**
+     * 微信公众号红包
+     * @param $openid 微信用户唯一标识
+     * @param $money 红包金额
+     * @param $cert 秘钥文件
+     * @param $sslKey 秘钥文件
+     * @param int $num 红包数量
+     * @param string $send_name 发送者
+     * @param string $action_name 发送原因
+     * @param string $description 红包描述
+     * @param string $wishing 祝福语
+     * @return array|bool 错误信息|是否成功
+     */
 	public static function Cash($openid,$money,$cert,$sslKey,$num=1,$send_name="",$action_name='恭喜发财',$description='红包可立即提现',$wishing='恭喜发财'){
 		# 判断要发送的红包金额是否小于1元
 		if($money < 1){
@@ -421,6 +377,15 @@ class Wechat{
 		# 返回发放结果
 		return $response->toArray();
 	}
+
+    /**
+     * 发送模板消息
+     * @param $Template_id 模板消息id
+     * @param $url 消息跳转url
+     * @param $openid 用户的openid
+     * @param $data 模板参数
+     * @return mixed 是否成功
+     */
 	public static function Template($Template_id,$url,$openid,$data){
 		# 判断是否已经获取过accessToken
 		if(self::$accessToken==false){
@@ -450,16 +415,12 @@ class Wechat{
 		# 返回发送结果
 		return $msgid;
 	}
-	# 发布菜单
+    /**
+     * 发布微信菜单
+     * @param $data 菜单数据
+     * @return Create 创建结果
+     */
 	public static function menu_create($data){
-		# $data = [
-		# 	['name'=>'个人中心','two'=>
-		# 		[['name'=>'我的订单','event'=>'view','val'=>'http://www.taobao.com'],
-		# 		['name'=>'我的钱包','event'=>'view','val'=>'http://www.tianmao.com']]
-		# 	],
-		# 	['name'=>'进入商城','event'=>'view','val'=>'kjskldhskashklhkl'],
-		# 	['name'=>'进入商城','event'=>'view','val'=>'kjskldhskashklhkl']
-		# ];
 		# 判断是否已经获取过accessToken
 		if(self::$accessToken==false){
 			# 获取accessToken
@@ -498,64 +459,62 @@ class Wechat{
 		# 返回结果
 		return $create;
 	}
-	/*
-	事件列表
-		subscribe 关注事件
-		scansubscribe 扫码二维码关注
-		scansubscribed 扫描二维码时已关注，直接进入会话事件
-		unsubscribe 取消关注事件
-		text 文本消息
-		image 图片消息
-		voice 语音消息
-		shortvideo 小视频消息事件
-		location 地理位置消息事件
-		link 链接消息事件
-		userlocation 上报地理位置事件
-		menuclick 自定义菜单点击拉取消息事件
-		menuview 自定义菜单跳转链接事件
-	 */
-	# 监听事件
+    /**
+     * 监听事件
+     * @param $event 事件
+     * @param $callback 回调方法
+     * @param array $value
+     */
 	public static function addEvent($event,$callback,$value=[]){
 		$result = self::xmlToArray($GLOBALS['HTTP_RAW_POST_DATA']);
 		if(strtolower($event) == strtolower($result['Event']) || (strtolower($event) == $result['MsgType']) ){
 			$callback($result,$value);
 		}
 	}
-	/**
-	 * [transfers 微信企业转账]
-	 * @return [type] [description]
-	 */
-	public function transfers($openid,$money,$cert,$sslKey,$num=1,$send_name="",$action_name='恭喜发财',$description='红包可立即提现',$wishing='恭喜发财'){
-		# 判断要发送的红包金额是否小于1元
-		if($money < 1){
-			# 发送的红包金额不能小于1元
-			return false;
-		}
-		# 初始化红包类
-		self::$cash = new Cash(C('appid','wechat'), C('mch_id','wechat'), C('key','wechat'));
+    /** 微信企业转账
+     * @param $openid 微信唯一识别标识
+     * @param $money 转账金额(最小1元)
+     * @param $cert 秘钥文件地址
+     * @param $sslKey 秘钥文件地址
+     * @param string $desc 描述
+     * @param string $check_name 是否检测收款人姓名
+     * @param string $re_user_name 收款人姓名
+     * @return bool
+     */
+    public static function transfersqiye($openid,$money,$cert,$sslKey,$desc='企业转账',$check_name='NO_CHECK',$re_user_name=''){
+        # 判断要发送的红包金额是否小于1元
+        if($money < 1){
+            # 发送的红包金额不能小于1元
+            return false;
+        }
+        # 初始化企业转账类
+        $transfers = new Transfers(C('appid','wechat'), C('mch_id','wechat'), C('key','wechat'));
 
-		# 现金红包必需设置证书
-		self::$cash->setSSLCert($cert,$sslKey);
+        # 企业转账必需设置证书
+        $transfers->setSSLCert($cert,$sslKey);
 
-		# 设置红包信息
-		self::$cash->set('send_name',     $send_name);# 发送者名称(一般都为公众号名称)
-		self::$cash->set('act_name',      $action_name);# 活动名称
-		self::$cash->set('remark',        $description);# 活动描述
-		self::$cash->set('wishing',       $wishing);# 祝福语
-		self::$cash->set('re_openid',     $openid);# 要发送给谁
-		self::$cash->set('total_amount',  $money * 100); # 发多少
-		self::$cash->set('total_num',     $num); # 发几个
-		self::$cash->set('mch_billno',    date('YmdHis').mt_rand(10000, 99999));
+        # 设置企业转账信息
+        $transfers->set('partner_trade_no',rand(0,20).date('YmdHisyysssi').rand(0,20));# 转账者
+        $transfers->set('openid',$openid);# 转账者
+        $transfers->set('check_name',$check_name);# 转账者
+        $transfers->set('re_user_name',$re_user_name);# 转账者
+        $transfers->set('amount',$money*100);# 转账者
+        $transfers->set('desc',$desc);# 转账者
+        $transfers->set('spbill_create_ip',$_SERVER['SERVER_ADDR']);# 转账者
 
-		try {
-		    $response = self::$cash->getResponse();
-		} catch (\Exception $e) {
-		    exit($e->getMessage());
-		}
-		# 返回发放结果
-		return $response->toArray();
-	}
-	# 通过openid获取userInfo
+        try {
+            $response = $transfers->getResponse();
+        } catch (\Exception $e) {
+            exit($e->getMessage());
+        }
+        # 返回发放结果
+        return $response->toArray();
+    }
+    /**
+     * 通过openid获取用户信息
+     * @param $openid 用户的openid
+     * @return Array 用户信息
+     */
 	public static function get_openid_user_info($openid){
 		# 判断是否已经获取过accessToken
 		if(self::$accessToken==false){
@@ -575,8 +534,12 @@ class Wechat{
 		self::$userinfo['nickname'] = Util::filterNickname(self::$userinfo['nickname']);
 		return self::$userinfo;
 	}
-	# Xml转Array
-	public static function xmlToArray($xml){
+    /**
+     * XML 转数组
+     * @param $xml XML
+     * @return mixed 数组
+     */
+	protected static function xmlToArray($xml){
 		#禁止引用外部xml实体
         libxml_disable_entity_loader(true);
         # Xml -> Object -> Json -> Array
@@ -585,8 +548,12 @@ class Wechat{
         # 返回结果
         return $values;
 	}
-	# Array转Xml
-	public static function arrayToXml($arr){
+    /**
+     * 数组转XML
+     * @param $arr 数组
+     * @return string XML
+     */
+	protected static function arrayToXml($arr){
 		# XML头
 		$xml = "<xml>";
         foreach ($arr as $key=>$val){
@@ -600,7 +567,10 @@ class Wechat{
         $xml.="</xml>";
         return $xml;
 	}
-	# Token 验证
+    /**
+     * Token 回调验证
+     * @param string $token 微信设置的Token
+     */
 	public static function checkToken($token=''){
 		if($token==''){
 			$token = self::$token;
@@ -619,5 +589,34 @@ class Wechat{
 	      exit($_GET["echostr"]);
 	    }
 	}
+    /**
+     * 获取参数签名；
+     * @param  Array  要传递的参数数组
+     * @return String 通过计算得到的签名；
+     */
+    protected static function getSign($params) {
+        #将参数数组按照参数名ASCII码从小到大排序
+        ksort($params);
+        # 循环处理要签名的数组
+        foreach ($params as $key => $item) {
+            # ß剔除参数值为空的参数
+            if (!empty($item)) {
+                # 整合新的参数数组
+                $newArr[] = $key.'='.$item;
+            }
+        }
+        # 使用 & 符号连接参数
+        $stringA = implode("&", $newArr);
+        # 拼接key
+        # key是在商户平台API安全里自己设置的
+        $stringSignTemp = $stringA."&key=".C('openid_pay_key','wechat');
+
+        # 将字符串进行MD5加密
+        $stringSignTemp = MD5($stringSignTemp);
+        # 将所有字符转换为大写
+        $sign = strtoupper($stringSignTemp);
+        # 返回签名结果
+        return $sign;
+    }
 
 }
