1eacc69a4e7275ee6f25f59af9e983a6b5b7281d
刘广财 <itxiao6@qq.com>
2017-07-10 18:45:37 +0800

    版本控制更新

--- service/View.php
+++ service/View.php
@@ -10,21 +10,21 @@ use Xiaoler\Blade\Factory;
 class View{
     # 获取视图实例
     public static function getView(){
-      # 循环处理模板目录
-      foreach(C('view_path','sys') as $value){
-        # 判断模板目录是否存在
-        if( !file_exists($value) ){
-            throw new \Exception("模板目录不存在或没有权限".$value);
+        # 循环处理模板目录
+        foreach(C('view_path','sys') as $value){
+            # 判断模板目录是否存在
+            if( !file_exists($value) ){
+                throw new \Exception("模板目录不存在或没有权限".$value);
+            }
         }
-      }
-      # 判断模板编译目录是否存在并且有写入的权限
-      if( (!file_exists(CACHE_VIEW)) or (!is_writable(CACHE_VIEW)) ){
-          throw new \Exception("模板编译目录不存在或没有权限");
-      }
-      $compiler = new BladeCompiler(CACHE_VIEW);
-      $engine = new CompilerEngine($compiler);
-      $finder = new FileViewFinder(C('view_path','sys'),C('extensions','sys'));
-      $factory = new Factory($engine,$finder);
-      return $factory;
+        # 判断模板编译目录是否存在并且有写入的权限
+        if( (!file_exists(CACHE_VIEW)) or (!is_writable(CACHE_VIEW)) ){
+            throw new \Exception("模板编译目录不存在或没有权限");
+        }
+        $compiler = new BladeCompiler(CACHE_VIEW);
+        $engine = new CompilerEngine($compiler);
+        $finder = new FileViewFinder(C('view_path','sys'),C('extensions','sys'));
+        $factory = new Factory($engine,$finder);
+        return $factory;
     }
 }
