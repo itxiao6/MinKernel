6e159a31cc23f780338716bc1968982027293583
戒尺 <itxiao6@qq.com>
2017-08-17 11:22:15 +0800

    抽象 缓存类

--- service/Cache.php
+++ service/Cache.php
@@ -1,6 +1,8 @@
 <?php
 namespace Service;
-
+use Doctrine\Common\Cache\RedisCache;
+use Doctrine\Common\Cache\FilesystemCache;
+use Redis;
 /**
  * 缓存类
  * Class Cache
@@ -14,10 +16,45 @@ class Cache{
     protected static $cacheDriver = false;
 
     /**
+     * 缓存驱动初始化
+     */
+    public static function __init(){
+        # 判断驱动是否初始化过
+        if(self::$cacheDriver==false){
+            # 选择缓存类型
+            switch(C('type','cache')){
+                case 'redis' :
+                    # redis驱动初始化
+                    # 实例化系统Redis 扩展
+                    $redis = new Redis();
+                    # 链接redis
+                    $redis -> connect(C('host','redis'), C('port','redis'));
+                    # 实例化缓存驱动
+                    $redis_driver = new RedisCache;
+                    # 设置缓存驱动的链接
+                    $redis_driver -> setRedis($redis);
+                    # 设置缓缓存驱动到Cache类
+                    self::setDriver($redis_driver);
+                    break;
+                case 'file' :
+                    # file驱动初始化
+                    # 实例化文件缓存驱动
+                    $redis_driver = new FilesystemCache(CACHE_DATA);
+                    # 设置驱动到Cache 类
+                    self::setDriver($redis_driver);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+    /**
      * 获取当前的缓存驱动
      * @return mixed
      */
     public static function getDriver(){
+        self::__init();
         return self::$cacheDriver;
     }
 
@@ -34,6 +71,7 @@ class Cache{
      * @return mixed
      */
     public static function set(){
+        self::__init();
         return self::$cacheDriver -> save(...func_get_args());
     }
 
@@ -42,6 +80,7 @@ class Cache{
      * @return mixed
      */
     public static function get(){
+        self::__init();
         return self::$cacheDriver -> fetch(...func_get_args());
     }
 
@@ -50,6 +89,7 @@ class Cache{
      * @return mixed
      */
     public static function remove(){
+        self::__init();
         return self::$cacheDriver -> delete(...func_get_args());
     }
 
@@ -61,6 +101,17 @@ class Cache{
      */
     public static function __callStatic($name, $arguments)
     {
+        self::__init();
+        return self::$cacheDriver -> $name(...$arguments);
+    }
+    /**
+     * 装饰者模式实现者
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     */
+    public static function __call($name, $arguments){
+        self::__init();
         return self::$cacheDriver -> $name(...$arguments);
     }
 }
