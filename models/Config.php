53241353c74be7e9f089bbbea13e46bdc886d331
戒尺 <itxiao6@qq.com>
2017-09-05 14:47:40 +0800

    配置模型

--- models/Config.php
+++ models/Config.php
@@ -9,6 +9,20 @@ use Kernel\Model;
  */
 class Config extends Model
 {
+    //--
+    //-- 表的结构 `config`
+    //--
+    //
+    //CREATE TABLE `config` (
+    //`id` int(11) NOT NULL,
+    //`name` varchar(50) NOT NULL COMMENT '键',
+    //`value` text NOT NULL COMMENT '值',
+    //`description` varchar(50) NOT NULL COMMENT '中文描述',
+    //`type` int(1) NOT NULL COMMENT '类型',
+    //`created_at` varchar(50) NOT NULL COMMENT '创建时间',
+    //`updated_at` varchar(50) NOT NULL COMMENT '最后更新时间',
+    //`deleted_at` varchar(50) DEFAULT NULL COMMENT '删除时间'
+    //) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='配置表';
     /**
      * 配置类型
      */
