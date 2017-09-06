a54e9591a681bc6587f9499e8fcce6f5df587a29
戒尺 <itxiao6@qq.com>
2017-09-06 10:19:30 +0800

    文件上传

--- service/Upload.php
+++ service/Upload.php
@@ -1,9 +1,11 @@
 <?php
 namespace Service;
 /**
- * 本地文件上传类
- * Class Upload
- * @package Service
++-----------------------------------------------------------------------------
+ * 文件上传类
++-----------------------------------------------------------------------------
+ * @author 刘广财
++-----------------------------------------------------------------------------
  */
 class Upload{
     # 定义上传文件夹
@@ -23,14 +25,14 @@ class Upload{
     # 上传后的文件名
     protected static $upload_file = [];
 
+
     # 处理图片
     public static function upload_one($file)
     {
         # 判断上传的目录是否存在
-        if(!file_exists(self::$folder)){
-            self::$error = 1;
-            self::$message = '上传目标文件夹不存在';
-            return false;
+        if(!is_dir(self::$folder)){
+            # 递归创建要上传的目标目录
+            mkdir(self::$folder,0777,true);
         }
         # 获取要上传的临时文件名称
         self::$tmp_file = $file['tmp_name'];
@@ -54,44 +56,44 @@ class Upload{
             return false;
         }
     }
-  # =============处理图片数组============
-  public static function uploads_array($file)
-  {
-    # 判断上传的目录是否存在
-    if(!file_exists(self::$folder)){
-      self::$error = 1;
-      self::$message = '上传目标文件夹不存在';
-      return false;
-    }
-    $picname=[];
-    foreach ($file['tmp_name'] as $key => $value) {
-      # 获取要上传的临时文件名称
-      self::$tmp_file = $value;
-      # 判断临时文件是否存在
-      if(!file_exists(self::$tmp_file)){
-        self::$error = 2;
-        self::$message = '上传的临时文件不存在';
-        return false;
-      }
-      foreach ($file['name'] as $k => $v) {
-             #找出对应的图片
-            if ($key==$k) {
-              # 获取文件后缀名
-              $suffix = substr($v,strrpos($v,'.'));
-              # 判断是否为合法文件
-              if(in_array($suffix,self::$suffix)){
-                self::$upload_file[0] = getRandomString(15);
-                # 上传文件
-                move_uploaded_file(self::$tmp_file,self::$folder.self::$upload_file[0].$suffix);
-                $picname[]= self::$web_folder.self::$upload_file[0].$suffix;
-              }else{
-                self::$error = 3;
-                self::$message = '文件类型错误';
+    # =============处理图片数组============
+    public static function uploads_array($file)
+    {
+        # 判断上传的目录是否存在
+        if(!file_exists(self::$folder)){
+            self::$error = 1;
+            self::$message = '上传目标文件夹不存在';
+            return false;
+        }
+        $picname=[];
+        foreach ($file['tmp_name'] as $key => $value) {
+            # 获取要上传的临时文件名称
+            self::$tmp_file = $value;
+            # 判断临时文件是否存在
+            if(!file_exists(self::$tmp_file)){
+                self::$error = 2;
+                self::$message = '上传的临时文件不存在';
                 return false;
-              }
-          }
-      }
-   }
-      return $picname;
-  }
+            }
+            foreach ($file['name'] as $k => $v) {
+                #找出对应的图片
+                if ($key==$k) {
+                    # 获取文件后缀名
+                    $suffix = substr($v,strrpos($v,'.'));
+                    # 判断是否为合法文件
+                    if(in_array($suffix,self::$suffix)){
+                        self::$upload_file[0] = getRandomString(15);
+                        # 上传文件
+                        move_uploaded_file(self::$tmp_file,self::$folder.self::$upload_file[0].$suffix);
+                        $picname[]= self::$web_folder.self::$upload_file[0].$suffix;
+                    }else{
+                        self::$error = 3;
+                        self::$message = '文件类型错误';
+                        return false;
+                    }
+                }
+            }
+        }
+        return $picname;
+    }
 }
