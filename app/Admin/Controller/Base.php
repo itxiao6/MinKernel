55b832486b5549863453ab1fa6c60a018fa33d40
戒尺 <itxiao6@qq.com>
2017-09-12 11:53:30 +0800

    模型修改

--- app/Admin/Controller/Base.php
+++ app/Admin/Controller/Base.php
@@ -49,7 +49,8 @@ class Base extends Controller{
         $right = explode(',',$roles -> right);
         # 获取结点
         $admin_right = AdminRight::whereIn('id',$right) -> pluck('node','id');
-        dd(AdminNode::where(['controller_name'=>$controller,'action_name'=>$action]) -> get());
+        dump($admin_right);
+        dd(AdminNode::where(['controller_name'=>$controller,'action_name'=>$action]) -> get() -> toArray());
         # 获取结点
         if(AdminNode::where(['controller_name'=>$controller,'action_name'=>$action]) -> whereIn('id',$admin_right) -> first()){
             return true;
