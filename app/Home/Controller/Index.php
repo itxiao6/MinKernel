69e6164e27c2020bd5c50059c7b126efaa0c0794
戒尺 <itxiao6@qq.com>
2017-09-21 15:01:10 +0800

    控制器注释规范

--- app/Home/Controller/Index.php
+++ app/Home/Controller/Index.php
@@ -1,9 +1,10 @@
 <?php
 namespace App\Home\Controller;
-use Kernel\Controller;
 /**
-* 首页控制器
-*/
+ * 前台首页控制器
+ * Class Index
+ * @package App\Home\Controller
+ */
 class Index extends Base{
   # 首页操作
   public function index(){
